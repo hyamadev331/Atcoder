1bb8170dacf2ef79e692d496428e7dfa26d491a2
hyamadev331 <hyamadev331@gmail.com>
2023-06-21 22:26:29 +0900

    Solve Problem

---
+++ ABC117/C.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (ll i = 0; i < ll(n); i++)
+#define reps(i, n) for (ll i = 1; i < ll(n); i++)
+#define rrep(i, n) for (ll i = ll(n); i >= 0; i--)
+#define ALL(x) x.begin(), x.end()
+#define FOR(i, a, b) for (ll i = a; i <= b; i++) // 使いにくかったら消す
+#define len(x) x.length()                        // 配列の長さ
+#define SIZE(x) x.size()                         // namestorのサイズ
+#define NPOS string::npos                        // findで検索失敗した場合の戻り値(findは文字列内に特定の文字列があるかを判定)
+typedef long long ll;
+using namespace std;
+const ll INF = 1LL << 60; // 無限大
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> x(m);
+    rep(i, m)
+    {
+        cin >> x[i];
+    }
+
+    sort(x.begin(), x.end());
+    vector<int> diff;
+    rep(i, m - 1)
+    {
+        diff.push_back(x[i + 1] - x[i]);
+    }
+
+    sort(diff.begin(), diff.end());
+    int ans = 0;
+    rep(i, SIZE(diff) - (n - 1))
+    {
+        ans += diff[i];
+    }
+    cout << ans << endl;
+
+    return 0;
+}
